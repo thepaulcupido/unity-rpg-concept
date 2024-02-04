732859b456f4c1d1ecb475ffcdb53c8f52dfd48b
Paul Cupido <35193740+PaulTouchFoundry@users.noreply.github.com>
2024-02-04 15:31:15 +0200

    Bound the Camera within the map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     private Transform target;
+    private Vector3 mapLimitTopRight;
+    private Vector3 mapLimitBottomLeft;
+
+    private float halfCameraHeight;
+    private float halfCameraWidth;
+
+
+    [SerializeField]
+    private Tilemap sceneTileMap;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        this.mapLimitTopRight = this.sceneTileMap.localBounds.max;
+        this.mapLimitBottomLeft = this.sceneTileMap.localBounds.min;
 
-        if (PlayerController.instance != null) {
+        if (Camera.main != null) {
+            this.halfCameraHeight = Camera.main.orthographicSize;
+            this.halfCameraWidth = this.halfCameraHeight * Camera.main.aspect;
+
+            //Setting the Camera limits in the context of the assigned TileMap and Camera size
+            this.mapLimitTopRight -=  new Vector3(this.halfCameraWidth, this.halfCameraHeight, 0f);
+            this.mapLimitBottomLeft += new Vector3(this.halfCameraWidth, this.halfCameraHeight, 0f);
+        } else {
+            Debug.Log("Main camera is not tagged as 'MainCamera' in the scene.");
+        }
+
+        
+        if (PlayerController.instance != null && target == null) {
+            // Centering the camera on the player
             target = PlayerController.instance.transform;
         }
     }
@@ -19,8 +45,12 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
-        if (target == null && PlayerController.instance != null) {
-            target = PlayerController.instance.transform;
-        } 
+
+        // Keeps the camera inside the bounds of the screen
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, this.mapLimitBottomLeft.x, this.mapLimitTopRight.x), 
+            Mathf.Clamp(transform.position.y, this.mapLimitBottomLeft.y, this.mapLimitTopRight.y), 
+            transform.position.z
+        );
     }
 }
