a6409271636c583f57996382df59c9038032a06f
Paul Cupido <43497530+thepaulcupido@users.noreply.github.com>
2026-05-26 22:38:35 +0200

    feat: add Shopkeeper NPC and configure sorting layers in Shop scene
    - Add Shopkeeper GameObject with SpriteRenderer on Characters sorting layer
    - Assign correct sorting layer to TilemapRenderer
    - Initialise `itemsForSale` to empty array on both Shop component instances
    - Pre-populate one instance with Wooden Sword and Leather Armour

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +7,16 @@ public class Shop : MonoBehaviour
 {
     public static Shop instance;
 
+    [Header ("Shop Menus")]
     [SerializeField] private GameObject shopMenu;
     [SerializeField] private GameObject buyMenu;
     [SerializeField] private GameObject sellMenu;
 
+     [Header ("Player Information")]
     [SerializeField] private Text goldText;
+
+     [Header ("Shop Items")]
+    [SerializeField] private string[] itemsForSale;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,21 +39,42 @@ public class Shop : MonoBehaviour
         goldText.text = GameManager.instance.GetCurrentGold().ToString() + "g";
     }
 
+    /// <summary>
+    /// This method closes the shop but setting the shop menu to inactive and calling the Game Manager's CloseShop method
+    /// </summary>
     public void CloseShop()
     {
         shopMenu.SetActive(false);
         GameManager.instance.CloseShopMenu();
     }
 
+    /// <summary>
+    /// This method sets the buy menu to active and the sell menu to inactive.
+    /// </summary>
     public void OpenBuyMenu()
     {
         buyMenu.SetActive(true);
         sellMenu.SetActive(false);
     }
 
+    /// <summary>
+    /// This method sets the sell menu to active and the buy menu to inactive.
+    /// </summary>
     public void OpenSellMenu()
     {
         buyMenu.SetActive(false);
         sellMenu.SetActive(true);
+    }
+
+    public GameObject GetShopMenu() => shopMenu;
+
+    public void SetItemsForSale(string[] newItems)
+    {
+        itemsForSale = new string[newItems.Length];
+
+        for (int i = 0; i < itemsForSale.Length; i++)
+        {
+            itemsForSale[i] = newItems[i];
+        }
     }
 }
